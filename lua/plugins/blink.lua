85119253a599874287a0326b438fe7a47a9fe271
Henner <henner@mitschwimmer.de>
2025-07-13 09:49:44 +0200

    Remove DAP and extra plugins

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -24,13 +24,8 @@ return {
                     -- per_filetype = {
                     --     codecompanion = { "codecompanion" },
                     -- },
-                    default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+                    default = { "lsp", "path", "snippets", "buffer" },
                     providers = {
-                        lazydev = {
-                            name = "LazyDev",
-                            module = "lazydev.integrations.blink",
-                            score_offset = 100,
-                        },
                         cmdline = {
                             min_keyword_length = 2,
                         },
